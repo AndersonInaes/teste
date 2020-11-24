c3d65db43373b88cbf361b47a05b7ba38e8a8ed0
AndersonInaes <47017403+AndersonInaes@users.noreply.github.com>
2020-11-24 16:45:41 -0300

    Create validacaoString.php

---
+++ validacaoString.php
@@ -0,0 +1,15 @@
+<?php 
+
+$variavel = "Romarinho";
+ 
+if(is_string($variavel)):
+    echo "É do tipo String.";
+else:
+    echo "Não é do tipo String.";
+endif; 
+ 
+// Saída É do tipo String
+
+
+
+ ?>
